105ce361f7b9ed2ca245e8c0ea8a83a8c3a41914
Cap'n Duck <coder@capnduck.com>
2023-12-22 11:39:54 -0800

    Updating tags.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "secret" {
-  source = "../secret"
-  #source = "git::https://github.com/CapnDucks/aws_secret?ref=v1.0.0"
+  #source = "../secret"
+  source = "git::https://github.com/CapnDucks/aws_secret?ref=1.1"
 
   db_admin_user          = var.db_admin_user
   secrets_manager_secret = var.secretsmanager_secret
