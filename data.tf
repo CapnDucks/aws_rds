6fc59505da17068ea6d1ecf1f939f1018240a7e7
Cap'n Duck <coder@capnduck.com>
2023-12-27 10:33:50 -0800

    .

--- data.tf
+++ data.tf
@@ -4,32 +4,3 @@ data "aws_vpc" "this" {
     values = [var.vpc_name]
   }
 }
-
-data "aws_subnets" "this" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.this.id]
-  }
-}
-
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.this.id]
-  }
-
-  tags = {
-    Name = "*private*"
-  }
-}
-
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.this.id]
-  }
-
-  tags = {
-    Name = "*public*"
-  }
-}
