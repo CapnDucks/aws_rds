882bc94a2cf0957451d028c171f285ad74e8bd12
Cap'n Duck <42878045+CapnDucks@users.noreply.github.com>
2023-12-27 13:08:40 -0800

    Updating to v1 tag instead of v1.0.0 (#10)
    * Updating to v1 tag instead of v1.0.0
    
    * Updating so terraform plan passes without any input.
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: Cap'n Duck <coder@capnduck.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-  db_identifier_prefix         = lower(replace(var.db_identifier_prefix, "_", "-"))  # only lowercase alphanumeric characters and hyphens allowed in "identifier_prefix"
-  db_final_snapshot_identifier = replace(var.db_final_snapshot_identifier, "_", "-") # must only contain alphanumeric characters and hyphens
+  db_identifier_prefix         = try(lower(replace(var.db_identifier_prefix, "_", "-")))  # only lowercase alphanumeric characters and hyphens allowed in "identifier_prefix"
+  db_final_snapshot_identifier = try(replace(var.db_final_snapshot_identifier, "_", "-")) # must only contain alphanumeric characters and hyphens
 
   tags = {
     terraform = "true"
