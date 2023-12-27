66e7da9ba243b02327e3fa8a3dee2ae7feaed333
Cap'n Duck <coder@capnduck.com>
2023-12-27 10:30:36 -0800

    .

--- variables.tf
+++ variables.tf
@@ -64,26 +64,53 @@ variable "db_deletion_protection" {
 }
 
 variable "db_subnet_group_name" {
-  default = "private"
+  default     = "private"
+  description = ""
+  type        = string
 }
 
 variable "db_multi_az" {
-  default = false
+  default     = false
+  description = ""
+  type        = bool
 }
 
-variable "db_vpc_security_group_ids" {}
+variable "db_vpc_security_group_ids" {
+  default     = null
+  description = ""
+  type        = string
+
+}
 
 variable "db_backup_retention_period" {
-  default = 7
+  default     = 7
+  description = ""
+  type        = string
 }
 
-variable "db_name" {}
+variable "db_name" {
+  default     = null
+  description = ""
+  type        = string
+}
 
-variable "db_final_snapshot_identifier" {}
+variable "db_final_snapshot_identifier" {
+  default     = null
+  description = ""
+  type        = string
+}
 
-variable "db_admin_user" {}
+variable "db_admin_user" {
+  default     = null
+  description = ""
+  type        = string
+}
 
-variable "vpc_name" {}
+variable "vpc_name" {
+  default     = null
+  description = ""
+  type        = string
+}
 
 variable "ca_cert_identifier" {
   default     = "rds-ca-rsa2048-g1"
